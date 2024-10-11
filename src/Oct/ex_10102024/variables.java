9fe6b5d920aa79a8eb6061bb7a9e5880dabdbf77
RishiD93 <rishid9093@gmail.com>
2024-10-11 11:30:01 -0400

    10-10-2024

---
+++ src/Oct/ex_10102024/variables.java
@@ -0,0 +1,37 @@
+package Oct.ex_10102024;
+
+public class variables {
+    public static void main(String[] args) {
+         //data types
+
+        byte byteVar = 127;          // 8-bit signed integer
+        short shortVar = 32767;      // 16-bit signed integer
+        int intVar = 2147483647;     // 32-bit signed integer
+        long longVar = 9223372036854775807L; // 64-bit signed integer
+
+        // Floating-point Types
+        float floatVar = 3.14f;      // 32-bit floating-point
+        double doubleVar = 3.141592653589793; // 64-bit floating-point
+
+        // Character Type
+        char charVar = 'A';          // 16-bit Unicode character
+
+        // Boolean Type
+        boolean boolVar = true;      // true or false
+
+        // Print all values
+        System.out.println("Byte value: " + byteVar);
+        System.out.println("Short value: " + shortVar);
+        System.out.println("Int value: " + intVar);
+        System.out.println("Long value: " + longVar);
+        System.out.println("Float value: " + floatVar);
+        System.out.println("Double value: " + doubleVar);
+        System.out.println("Char value: " + charVar);
+        System.out.println("Boolean value: " + boolVar);
+
+
+        //
+
+
+    }
+}
