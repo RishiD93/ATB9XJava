f44285f2b2e79e3d86160e334589b95c964c5f6b
RishiD93 <rishid9093@gmail.com>
2024-10-11 12:08:57 -0400

    10-10-2024

--- src/Oct/ex_10102024/variables.java
+++ src/Oct/ex_10102024/variables.java
@@ -1,8 +1,15 @@
 package Oct.ex_10102024;
 
 public class variables {
+
+    // Instance variable (belongs to the instance of the class)
+    int instanceVariable = 10;
+
+    // Static variable (belongs to the class)
+    static String staticVariable = "Hello, I am a static variable!";
+
     public static void main(String[] args) {
-         //data types
+        // Data types
 
         byte byteVar = 127;          // 8-bit signed integer
         short shortVar = 32767;      // 16-bit signed integer
@@ -29,9 +36,30 @@ public class variables {
         System.out.println("Char value: " + charVar);
         System.out.println("Boolean value: " + boolVar);
 
+        // Local variable (declared inside a method)
+        int localVariable = 5;
 
-        //
+        // Print the local variable
+        System.out.println("Local Variable: " + localVariable);
 
+        // Print the static variable directly
+        System.out.println("Static Variable: " + staticVariable);
+
+        // Creating an object to access instance variables
+        variables example = new variables();
+        System.out.println("Instance Variable: " + example.instanceVariable);
+
+        // Constant variable (final keyword makes it a constant)
+        final double PI = 3.14159;
+        System.out.println("Constant PI: " + PI);
+
+        // Array variable (storing multiple values)
+        int[] arrayVariable = {1, 2, 3, 4, 5};
+        System.out.println("Array Variable (First element): " + arrayVariable[0]);
+
+        // String variable (Object)
+        String stringVariable = "Java Programming";
+        System.out.println("String Variable: " + stringVariable);
 
     }
 }
