32f081fa11bae77e3f1f9a1cfaf87170dc7ff0cb
RishiD93 <rishid9093@gmail.com>
2024-10-23 11:56:44 -0400

    11-18-2024

---
+++ src/Oct/ex_11182024/Switchdemo.java
@@ -0,0 +1,38 @@
+package Oct.ex_11182024;
+
+import java.util.Scanner;
+
+public class Switchdemo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enyrt the day from i to 10");
+        int day =scanner.nextInt();
+        switch (day){
+            case 1:
+                System.out.println("Monday");
+                break;
+
+            case  2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("No idea, what day it is");
+                break;
+        }
+    }
+}
