63c384639581eac8e70b61d51a155ab103604a4e
RishiD93 <rishid9093@gmail.com>
2024-10-31 20:58:29 -0400

    10-23-2024

---
+++ src/Oct/ex_10212024/forloopdemo.java
@@ -0,0 +1,10 @@
+package Oct.ex_10212024;
+
+public class forloopdemo {
+    public static void main(String[] args) {
+         for(int i =0; i<10;i++)
+         {
+             System.out.println(i);
+         }
+    }
+}
---
+++ src/Oct/ex_31102024/Lab1.java
@@ -0,0 +1,18 @@
+package Oct.ex_31102024;
+public class Lab1 {
+    public class Main {
+        public static void main(String[] args) {
+            for (int i = 0; i < 10; i++) {  // Declare i as an int
+                System.out.println(i);
+            }
+
+            for (int a = 0; a <= 10; a++) {
+                System.out.println(a);
+            }
+
+            for (char c = 'a'; c <= 'z'; c++) {  // Use char for character iteration
+                System.out.println(c);
+            }
+        }
+    }
+}
